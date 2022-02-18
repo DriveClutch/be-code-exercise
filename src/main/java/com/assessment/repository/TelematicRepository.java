d1fd1d63d884e68559976ef3e148da67e86c918c
Cassendra4 <hasangi44@hotmail.com>
2022-02-18 16:05:17 -0500

    repository changes

--- src/main/java/com/assessment/repository/TelematicRepository.java
+++ src/main/java/com/assessment/repository/TelematicRepository.java
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TelematicRepository extends CrudeRepository<TelematicEntity, RequestEntity> {
-    Optional<TelematicEntity> findAllByVehicleEntityVinAndEventTimestampBetween(String vin, Timestamp start, Timestamp end);
+    List<TelematicEntity> findAllByVehicleEntityVinAndEventTimestampBetween(String vin, Timestamp start, Timestamp end);
     //select * from temelatic where vin = ? and event_timestamp between ? and ?
-    List<TelematicEntity> findFirstByVehicleEntityVinAndModelAndMakeAndColorOderByRequestEntityEventTimestampDesc(String vin, Timestamp start, Timestamp end);
-    //select * from telematic where vin = ? and model = ? and make = ? and color=? order by event_timestamp desc;
+    Optional<TelematicEntity> findFirstByVehicleEntityVinAndModelAndMakeAndColorOderByRequestEntityEventTimestampDesc(String vin, String model, 
+    String make, String color);
+    //select * from telematic where vin = ? and model = ? and make = ? and color=? order by event_timestamp desc limit 1;
 }
