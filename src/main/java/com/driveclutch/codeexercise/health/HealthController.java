53b56cee3229fbbead03b19cc735cf70cc4f44fe
Lisa Romero <lisa@driveclutch.com>
2022-02-18 12:10:05 -0500

    Initial commit

---
+++ src/main/java/com/driveclutch/codeexercise/health/HealthController.java
@@ -0,0 +1,13 @@
+package com.driveclutch.codeexercise.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("ping")
+    public static String ping() {
+        return "pong";
+    }
+}
---
+++ src/main/java/com/driveclutch/codeexercise/users/User.java
@@ -0,0 +1,59 @@
+package com.driveclutch.codeexercise.users;
+
+import org.joda.time.DateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private DateTime createdAt;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.createdAt = DateTime.now();
+    }
+
+    public User() {
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public DateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(DateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+}
